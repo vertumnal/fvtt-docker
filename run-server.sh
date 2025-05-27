a7569835444e1ef84e90bda04bd2cb2253b0f21e
Ryan <ultimatealien600@gmail.com>
2025-05-27 19:59:09 +0800

    Added step to delete everything in resources/app to ensure that the directory is empty before moving the new files

--- run-server.sh
+++ run-server.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# look for a file name foundryvtt-.....zip or FoundryVTT...zip, copy it to 
-# /opt/foundryvtt, unzip and remove it. Then start the foundry app
+set -e  # Exit immediately on error
 
 cd /opt/foundryvtt || exit
 echo "Copying zip file..."
@@ -10,9 +9,12 @@ find /host -type f -name '[f,F]oundry[vtt,VTT]*.zip' -exec cp '{}' . ';'
 echo "Unzipping..."
 unzip -o ./*.zip -d unpacked && rm ./*.zip
 
-echo "Moving to mapped directory"
+echo "Cleaning target app folder..."
+rm -rf resources/app/*
+
+echo "Moving to mapped directory..."
 mv unpacked/* resources/app/
 
-echo "Starting node"
+echo "Starting node..."
 cd resources/app
-node main.mjs --dataPath=/data/foundryvtt
+exec node main.mjs --dataPath=/data/foundryvtt
